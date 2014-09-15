eed7c9a7a06bbdd59bf7a545f7eddcbf7a26e9d1
Duke <duke@bart>
2014-09-16 01:56:39 +0200

    asdf

--- htdocs/lib/libFrontend.php
+++ htdocs/lib/libFrontend.php
@@ -2,7 +2,8 @@
 
 function showStartPage () {
    global $uid, $userid;
-   echo getHeader($menu = 'all');
+   $explodedRequest = explode('/', $_SERVER['REQUEST_URI']);
+   echo getHeader($menu = $explodedRequest[0]);
    echo '<br><p>Willkommen, ' . $uid . ' (skm #' . $userid . '), bei der Schl&uuml;sselverwaltung.</p>';
 
    echo getFooter();
@@ -79,7 +80,7 @@ ul#Navigation a#Aktiv {
 </head>
 <body id="seite" bgcolor="#FFFFFF" link="black" vlink="black" alink="red">
    <table width="80%" border="0" align="center">
-   <tr align="center"><td><h1>skeymanager - dev</h1></td></tr>' . getMenu(explode('/', $_SERVER['REQUEST_URI'])[0]) . '   <tr align="center"><td>
+   <tr align="center"><td><h1>skeymanager - dev</h1></td></tr>' . getMenu() . '   <tr align="center"><td>
 ';
 
    return $result;
