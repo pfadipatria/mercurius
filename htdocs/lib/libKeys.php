117dee696f448bc8d5e1d25e603d79a02ef6fbcb
Duke <duke@bart>
2014-09-16 12:02:52 +0200

    asdf

--- htdocs/lib/libKeys.php
+++ htdocs/lib/libKeys.php
@@ -13,11 +13,12 @@ function showKeyListPage(){
    echo getHeader('keys', 'list');
    echo '<br><p>Hier ist eine &Uuml;bersicht aller Schl&uumlssel.</p>';
    // echo getKeyList();
-  $query = 'select doorkey.id,elnumber,code,doorkeycolor.name AS colorname,doorkeytatus.name AS statusname,doorkeymech.bezeichung AS bezeichung,comment from doorkey JOIN doorkeycolor ON (doorkey.color = doorkeycolor.id ) JOIN doorkeytatus ON (doorkey.status = doorkeytatus.id) JOIN doorkeymech ON (doorkey.mechnumber = doorkeymech.id ) LIMIT 10';
+  # $query = 'select doorkey.id,elnumber,code,doorkeycolor.name AS colorname,doorkeytatus.name AS statusname,doorkeymech.bezeichung AS bezeichung,comment from doorkey JOIN doorkeycolor ON (doorkey.color = doorkeycolor.id ) JOIN doorkeytatus ON (doorkey.status = doorkeytatus.id) JOIN doorkeymech ON (doorkey.mechnumber = doorkeymech.id ) LIMIT 10';
+   $query = 'select * from doorkey limit 10';
    $con = openDb();
    $dbresult = queryDb($con, $query);
 	while ($row = mysqli_fetch_array($dbresult)){
-      var_dump($row);
+      echo '<p>' . print_r($row) . '</p>';
    }
    echo getFooter();
 }
