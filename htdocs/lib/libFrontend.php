991c556b568ae428e3929a2077c30d2fc34fe50c
Duke <duke@bart>
2014-09-16 02:08:17 +0200

    asdf

--- htdocs/lib/libFrontend.php
+++ htdocs/lib/libFrontend.php
@@ -3,6 +3,7 @@
 function showStartPage () {
    global $uid, $userid;
    $explodedRequest = explode('/', $_SERVER['REQUEST_URI']);
+   var_dump($explodedRequest);
    echo 'Current page is ' . $explodedRequest['0'];
    echo getHeader('keys');
    echo '<br><p>Willkommen, ' . $uid . ' (skm #' . $userid . '), bei der Schl&uuml;sselverwaltung.</p>';
