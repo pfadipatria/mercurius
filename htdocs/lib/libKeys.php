3f8b4ba9de88f68b5db2b9e4921197330f6e5f34
Duke <duke@bart>
2014-09-16 12:27:51 +0200

    asdf

--- htdocs/lib/libKeys.php
+++ htdocs/lib/libKeys.php
@@ -12,24 +12,24 @@ function showKeysPage(){
 function showKeyListPage(){
    echo getHeader('keys', 'list');
    echo '<br><p>Hier ist eine &Uuml;bersicht aller Schl&uumlssel.</p>';
-   echo getKeyList();
+   printKeyList();
    echo getFooter();
 }
 
-function getKeyList(){
+function printKeyList(){
    $result = '';
 
-   $result .= '<table>';
+   echo '<table>';
    // $query = 'select doorkey.id,elnumber,code,doorkeycolor.name AS colorname,doorkeytatus.name AS statusname,doorkeymech.bezeichung AS bezeichung,comment from doorkey JOIN doorkeycolor ON (doorkey.color = doorkeycolor.id ) JOIN doorkeytatus ON (doorkey.status = doorkeytatus.id) JOIN doorkeymech ON (doorkey.mechnumber = doorkeymech.id ) LIMIT 10';
-   $query = 'select * from doorkey';
+   $query = 'select * from doorkey limit 10';
    $con = openDb();
    $dbresult = queryDb($con, $query);
 	while ($row = mysqli_fetch_array($dbresult)){
-      $result .= '<tr><td>' . $row . ' elnumber: ' . $row['elnumber'] . ' comment: ' . $row['comment'] . '</td></tr>';
+      echo '<tr><td>' . $row . ' elnumber: ' . $row['elnumber'] . ' comment: ' . $row['comment'] . '</td></tr>';
    }
 
-   $result .= '</table>';
-   return $result;
+   echo '</table>';
+   // return $result;
 }
 
 ?>
