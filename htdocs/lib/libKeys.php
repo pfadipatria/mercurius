393546ce9e46934268031546f94d279ff4873dd6
Duke <duke@bart>
2014-09-16 12:06:28 +0200

    asdf

--- htdocs/lib/libKeys.php
+++ htdocs/lib/libKeys.php
@@ -12,23 +12,19 @@ function showKeysPage(){
 function showKeyListPage(){
    echo getHeader('keys', 'list');
    echo '<br><p>Hier ist eine &Uuml;bersicht aller Schl&uumlssel.</p>';
-   // echo getKeyList();
-  # $query = 'select doorkey.id,elnumber,code,doorkeycolor.name AS colorname,doorkeytatus.name AS statusname,doorkeymech.bezeichung AS bezeichung,comment from doorkey JOIN doorkeycolor ON (doorkey.color = doorkeycolor.id ) JOIN doorkeytatus ON (doorkey.status = doorkeytatus.id) JOIN doorkeymech ON (doorkey.mechnumber = doorkeymech.id ) LIMIT 10';
-   $query = 'select * from doorkey';
-   $con = openDb();
-   $dbresult = queryDb($con, $query);
-	while ($row = mysqli_fetch_array($dbresult)){
-      echo '<p>' . $row['elnumber'] . ' comment: ' . $row['comment'] . '</p>';
-   }
+   echo getKeyList();
    echo getFooter();
 }
 
 function getKeyList(){
    $result = '';
 
-   while ($key = getKeyFromDb()){
-      $result .= $key;
-
+  # $query = 'select doorkey.id,elnumber,code,doorkeycolor.name AS colorname,doorkeytatus.name AS statusname,doorkeymech.bezeichung AS bezeichung,comment from doorkey JOIN doorkeycolor ON (doorkey.color = doorkeycolor.id ) JOIN doorkeytatus ON (doorkey.status = doorkeytatus.id) JOIN doorkeymech ON (doorkey.mechnumber = doorkeymech.id ) LIMIT 10';
+   $query = 'select * from doorkey';
+   $con = openDb();
+   $dbresult = queryDb($con, $query);
+	while ($row = mysqli_fetch_array($dbresult)){
+      $result .= '<p>' . $row['elnumber'] . ' comment: ' . $row['comment'] . '</p>';
    }
 
    return $result;
