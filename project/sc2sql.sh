0c0221c6899fb58cc83788fb5da35d8f0ac269c1
Duke <duke@bart>
2014-09-16 14:59:55 +0200

    asdf

---
+++ project/sc2sql.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# cat schloesser.csv | ~/devel/skeymanager/project/sc2sql.sh
+
+while read line
+do
+   #echo "bearbeite $line"
+   heim="$( echo "${line}" | cut -d \; -f 1 | tr -d "\"" )"
+   name="$( echo "${line}" | cut -d \; -f 2 | tr -d "\"" | cut -d \   -f 1 )"
+   bez="$( echo "${line}" | cut -d \; -f 2 | tr -d "\"" | cut -d \   -f 2- )"
+   sc="$( echo "${line}" | cut -d \; -f 3 | tr -d "\"" | cut -d \. -f 2)"
+
+   # echo "Inserting heim: ${heim} name: ${name} bez: ${bez} sc: ${sc}"
+   # continue
+   echo "
+INSERT INTO doorlock (
+                     number,
+                     name,
+                     sc,
+                     place
+                    )
+                    VALUES
+                    (
+                     '${name}',
+                     '${bez}',
+                     '${sc}',
+                     (SELECT id FROM doorplace WHERE name = '${heim}')
+                    );
+"
+   
+done
