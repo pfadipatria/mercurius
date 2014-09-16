7bd438e0aad3dd3ba597ff495d43d6cabdaf6cfd
Duke <duke@bart>
2014-09-16 15:23:10 +0200

    asdf

--- htdocs/lib/libAll.php
+++ htdocs/lib/libAll.php
@@ -4,5 +4,6 @@ include 'libFrontend.php';
 include 'libUser.php';
 include 'libDb.php';
 include 'libKeys.php';
+include 'libLocks.php';
 
 ?>
---
+++ htdocs/lib/libLocks.php
@@ -0,0 +1,65 @@
+<?php
+
+function showLocksPage(){
+   switch(getMenuPath('2')){
+      case 'list':
+         showLockListPage();
+         break;
+      case 'show':
+         showLockDetailsPage(getMenuPath('3'));
+         break;
+      default:
+         showLockListPage();
+   }
+}
+
+function showLockListPage(){
+   echo getHeader('locks', 'list');
+   printLockList();
+   echo getFooter();
+}
+
+function printLockList(){
+
+   echo '<table cellpadding="5" cellspacing="0">';
+   echo '<tr>
+      <td>id</td>
+      <td>SC</td>
+      <td>Heim</td>
+      <td>Bezeichnung</td>
+      <td>Status</td>
+      <td>Kommentar</td>
+      </tr>';
+   $query = '
+      SELECT
+         doorlock.id AS lockid,
+         number,
+         doorlock.name AS lockname,
+         sc,
+         doorplace.name AS heim,
+         doorlockstatus.name AS statusname
+         FROM doorlock
+         LEFT JOIN doorplace ON (doorlock.place = doorplace.id)
+         LEFT JOIN doorlockstatus ON (doorlock.status = doorlockstatus.id)
+         ORDER BY sc
+         ';
+   // $query = 'select * from doorkey limit 10';
+   $con = openDb();
+   $dbresult = queryDb($con, $query);
+	while ($row = mysqli_fetch_array($dbresult)){
+      echo '<tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'" onclick="document.location = \'/locks/show/' . $row['lockid'] . '\';" style="cursor: zoom-in";>
+         <td>' . $row['lockid'] . '</td>
+         <td>' . $row['sc'] . '</td>
+         <td>' . $row['heim'] . '</td>
+         <td>' . $row['lockname'] . '(' . $row['number'] . ')</td>
+         <td>' . $row['statusname'] . '</td>
+         <td>' . $row['comment'] . '</td>
+         </tr>';
+   }
+
+   echo '</table>';
+
+}
+
+
+?>
