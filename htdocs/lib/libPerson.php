c5f7fd5f1edcacc362376636952ff938573d0b96
Hermelin <hermelin@pfadipatria.com>
2014-09-16 21:24:14 +0200

    asdf im mrr

--- htdocs/lib/libPerson.php
+++ htdocs/lib/libPerson.php
@@ -62,8 +62,18 @@ function showPersonDetailsPage($personId = '0'){
    echo '<p onclick="goBack()" style="cursor: pointer">Zur&uuml;ck</p>';
    printPersonDetails($personId);
    echo '<br><a href="/person/edit/' . $personId . '">Bearbeiten</a><br><p onclick="goBack()" style="cursor: pointer">Zur&uuml;ck</p><hr><h3>Schl&uuml;ssel:</h3><br>';
-   // printPersonKeys($personId);
+   printPersonKeys($personId);
    echo '<br><p onclick="goBack()" style="cursor: pointer">Zur&uuml;ck</p><br>';
+   echo getFooter();
+}
+
+function showPersonEditPage($personId = '0'){
+   echo getHeader('person', '');
+   echo '<br>';
+   printKeyEdit($personId);
+   echo '<br><br><hr><br><h3>Berechtigungen:</h3><br>';
+   printKeyPermissions($keyId);
+   echo '<br>';
    echo getFooter();
 }
 
@@ -99,25 +109,23 @@ function printPersonDetails($personId = '0'){
 function printPersonKeys($personId = '0'){
    echo '<table cellpadding="5" cellspacing="0">';
 
+   # Ich glaube hier brauchts einen RIGHT JOIN nicht LEFT
    $query = "
       SELECT
-         doorperson.id,
-         doorkey.code,
-         uid,
-         uidnumber,
-         mdbid
-         LEFT JOIN doorkey ON (doorperson.id = doorkey.owner)
+         doorkey.id AS keyid,
+         doorkey.code AS keycode
+         doorkey.comment AS keycomment
          FROM doorperson
+         LEFT JOIN doorkey ON (doorperson.id = doorkey.owner)
          WHERE id = '" . $personId . "'
       ";
    // error_log($query);
    $con = openDb();
    $dbresult = queryDb($con, $query);
 	while ($row = mysqli_fetch_array($dbresult)){
-      echo '<tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'" onclick="document.location = \'/locks/show/' . $row['lockid'] . '\';" style="cursor: zoom-in">
-               <td>SC ' . $row['locksc'] . '</td>
-               <td>' . $row['heim'] . '</td>
-               <td>' . $row['lockname'] . '</td>
+      echo '<tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'" onclick="document.location = \'/keys/show/' . $row['keyid'] . '\';" style="cursor: zoom-in">
+               <td>MC ' . $row['keycode'] . '</td>
+               <td>' . $row['keycomment'] . '</td>
             </tr>
          ';
    }
