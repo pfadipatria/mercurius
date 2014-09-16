0fc4fdf5354ec784c8ae78709ec3444b52ec4dc0
Duke <duke@bart>
2014-09-16 12:03:44 +0200

    asdf

--- htdocs/lib/libKeys.php
+++ htdocs/lib/libKeys.php
@@ -14,7 +14,7 @@ function showKeyListPage(){
    echo '<br><p>Hier ist eine &Uuml;bersicht aller Schl&uumlssel.</p>';
    // echo getKeyList();
   # $query = 'select doorkey.id,elnumber,code,doorkeycolor.name AS colorname,doorkeytatus.name AS statusname,doorkeymech.bezeichung AS bezeichung,comment from doorkey JOIN doorkeycolor ON (doorkey.color = doorkeycolor.id ) JOIN doorkeytatus ON (doorkey.status = doorkeytatus.id) JOIN doorkeymech ON (doorkey.mechnumber = doorkeymech.id ) LIMIT 10';
-   $query = 'select * from doorkey limit 10';
+   $query = 'select * from doorkey';
    $con = openDb();
    $dbresult = queryDb($con, $query);
 	while ($row = mysqli_fetch_array($dbresult)){
