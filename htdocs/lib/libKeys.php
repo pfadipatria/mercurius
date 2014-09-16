76055db4e6b7abd77f8f37178878da74c76b3f74
Duke <duke@bart>
2014-09-16 13:02:44 +0200

    asdf

--- htdocs/lib/libKeys.php
+++ htdocs/lib/libKeys.php
@@ -62,19 +62,18 @@ function printKeyDetails($keyId = '0'){
    echo '<table cellpadding="5" cellspacing="0">';
 
    $query = "select doorkey.id,elnumber,code,doorkeycolor.name AS colorname,doorkeytatus.name AS statusname,doorkeymech.bezeichung AS bezeichung,comment from doorkey JOIN doorkeycolor ON (doorkey.color = doorkeycolor.id ) JOIN doorkeytatus ON (doorkey.status = doorkeytatus.id) JOIN doorkeymech ON (doorkey.mechnumber = doorkeymech.id ) WHERE doorkey.id = '" . $keyId . "'";
-   error_log($query);
+   // error_log($query);
    $con = openDb();
    $dbresult = queryDb($con, $query);
 	while ($row = mysqli_fetch_array($dbresult)){
       echo '
          <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>id</td><td>' . $row['id'] . '</td></tr>
-         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>id</td><td>' . $row['elnumber'] . '</td></tr>
-         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>id</td><td>' . $row['code'] . '</td></tr>
-         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>id</td><td>' . $row['colorname'] . '</td></tr>
-         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>id</td><td>' . $row['statusname'] . '</td></tr>
-         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>id</td><td>' . $row['bezeichung'] . '</td></tr>
-         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>id</td><td>' . $row['comment'] . '</td></tr>
-         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>id</td><td><a href="/keys/show/' . $row['id'] . '">Show</a></td></tr>
+         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>ElNumber</td><td>' . $row['elnumber'] . '</td></tr>
+         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>Code</td><td><b>' . $row['code'] . '</b></td></tr>
+         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>Farbe</td><td>' . $row['colorname'] . '</td></tr>
+         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>Status</td><td>' . $row['statusname'] . '</td></tr>
+         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>Bezeichnung</td><td>' . $row['bezeichung'] . '</td></tr>
+         <tr onMouseOver="this.className=\'highlight\'" onMouseOut="this.className=\'normal\'"><td>Kommentar</td><td>' . $row['comment'] . '</td></tr>
          ';
    }
 
