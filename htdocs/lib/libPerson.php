74466d5a8572da28e5837ddaaeb0ccb4067c7245
Hermelin <hermelin@pfadipatria.com>
2014-09-17 03:51:31 +0200

    asdf im mrr

--- htdocs/lib/libPerson.php
+++ htdocs/lib/libPerson.php
@@ -86,7 +86,7 @@ function showPersonEditPage($personId = '0'){
     $addArray['mode'] = 'update';
     $addArray['id'] = $personId;
     foreach($_POST as $item => $value){
-        $addArray[$item] = $value;
+        if ($value != '' || $value != '0' || !empty(value)) $addArray[$item] = $value;
     }
     modifiyDbPerson($addArray);
    }
