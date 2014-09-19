ae8c28958a06cecbb74b2c5dc811b39ed471ac00
Hermelin <hermelin@pfadipatria.com>
2014-09-19 02:30:29 +0200

    asdf im mrr

--- htdocs/src/SKeyManager/Repository/AbstractRepository.php
+++ htdocs/src/SKeyManager/Repository/AbstractRepository.php
@@ -9,7 +9,7 @@ abstract class AbstractRepository {
     }
 
     protected function query($where = ' ') {
-        var_dump($this);
+        var_dump($this, $where);
         $con = openDb();
         $dbresult = queryDb($con, $this->select.$this->from.$where.$this->order);
         $rows = array();
