5ffe7aa8b9254788f98da6147a3c5e0f3c656023
Hermelin <hermelin@pfadipatria.com>
2014-09-20 16:40:27 +0200

    asdf im mrr

--- htdocs/lib/libFrontend.php
+++ htdocs/lib/libFrontend.php
@@ -109,12 +109,58 @@ $result = '
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <div class="navbar-brand">SKeyMangager - DEV</div>
+      <a class="navbar-brand" href="/">SKeyMangager - DEV</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li ' . $homeLink . '><a href="/"><span class="glyphicon glyphicon-home"></span></a></li>
+        <li class="dropdown">
+          <a href="#" ' . $keysLink . ' class="dropdown-toggle" data-toggle="dropdown">Schl&uuml;ssel <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="/key/">Liste</a></li>
+            <li><a href="/key/add">Hinzuf&uuml;gen</a></li>
+            <li><a href="/key/search">Suchen</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="/lock" ' . $locksLink . ' class="dropdown-toggle" data-toggle="dropdown">Schl&ouml;sser <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="/lock/add">Hinzuf&uuml;gen</a></li>
+            <li><a href="/lock/search">Suchen</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="/person" ' . $personLink . ' class="dropdown-toggle" data-toggle="dropdown">Personen</a>
+          <!-- <ul class="dropdown-menu" role="menu">
+            <li><a href="/person/add">Hinzuf&uuml;gen</a></li>
+            <li><a href="/person/search">Suchen</a></li>
+          </ul> -->
+        </li>
+        <li ' . $historyLink . '><a href="/history">Verlauf</a></li>
+
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li ' . $helpLink . '><a href="/help">Help</a></li>
+        <li><a href="/profile"><span class="glyphicon glyphicon-user"></a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<nav class="navbar" role="navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">SKeyMangager - DEV</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
         <li class="dropdown">
           <a href="#" ' . $keysLink . ' class="dropdown-toggle" data-toggle="dropdown">Schl&uuml;ssel <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
@@ -148,41 +194,6 @@ $result = '
   </div><!-- /.container-fluid -->
 </nav>
 ';
-
-   return $result;
-   $result .= '
-      <tr align="center"><td><ul id="Navigation">
-         <li><a href="/home"' . $homeLink . '>Home</a></li>
-         <li><a href="/keys/list"' . $keysLink . '>Schl&uuml;ssel</a></li>
-         <li><a href="/locks/list"' . $locksLink . '>Schl&ouml;sser</a></li>
-         <li><a href="/person/list"' . $personLink . '>Personen</a></li>
-         <li><a href="/history/list"' . $historyLink . '>Verlauf</a></li>
-         <li><a href="/help"' . $helpLink . '>Hilfe</a></li>
-      </ul>';
-
-   switch($menu){
-      case 'keys':
-      case 'locks':
-      case 'person':
-         $result .= '
-         <ul id="Navigation" style="border-top-color: silver;">
-            <li><a href="/' . $menu .'/list"' . $listLink . '>Liste</a></li>
-            <li><a href="/' . $menu .'/search"' . $searchLink . '>Suchen</a></li>
-            <li><a href="/' . $menu .'/add"' . $addLink . '>Hinzuf&uuml;gen</a></li>
-         </ul>';
-      break;
-      case 'history':
-         $result .= '
-         <ul id="Navigation" style="border-top-color: silver;">
-            <li><a href="/' . $menu .'/list"' . $listLink . '>Liste</a></li>
-            <li><a href="/' . $menu .'/search"' . $searchLink . '>Suchen</a></li>
-         </ul>';
-      break;
-   }
-
-   $result .= '
-      </td></tr>';
-
    return $result;
 }
 ?>
