75fd2e42874fcf7903a1761a1c8c1c5a1316430c
Hermelin <hermelin@pfadipatria.com>
2014-09-20 12:56:18 +0200

    asdf im mrr

--- htdocs/src/SKeyManager/Entity/PersonEntity.php
+++ htdocs/src/SKeyManager/Entity/PersonEntity.php
@@ -4,41 +4,42 @@ namespace SKeyManager\Entity;
 
 class PersonEntity extends AbstractEntity {
 
-    protected $locationPattern = '/person/show/%s';
-    var $id;
+   protected $locationPattern = '/person/show/%s';
+   var $id;
 
-    function __construct($id) {
-        $this->id = $id;
+   function __construct($id) {
+      error_log('id ist ' . $id)
+      $this->id = $id;
 
-        $this->select = '
-            SELECT
-                id,
-                name,
-                uid,
-                uidnumber,
-                mdbid,
-                comment,
-                lastupdate
-        ';
+      $this->select = '
+         SELECT
+             id,
+             name,
+             uid,
+             uidnumber,
+             mdbid,
+             comment,
+             lastupdate
+      ';
 
-        $this->from = '
-            FROM doorperson
-        ';
+      $this->from = '
+         FROM doorperson
+      ';
 
-        $this->where = '
-           WHERE id = ' . $id . '
-        ';
+      $this->where = '
+        WHERE id = ' . $id . '
+      ';
 
-        $this->order = '
-            ORDER BY name;
-        ';
-    }
+      $this->order = '
+         ORDER BY name;
+      ';
+   }
 
-    function getName() {
-        $this->select = 'SELECT name';
-        $row = parent::getAll();
-        return $row['name'];
-    }
+   function getName() {
+      $this->select = 'SELECT name';
+      $row = parent::getAll();
+      return $row['name'];
+   }
 
    function getKeys() {
 
