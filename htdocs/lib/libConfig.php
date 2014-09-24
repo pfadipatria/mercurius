86fb66acaf7f336e8c36fb1ba2a1341a055be9ce
Duke <duke@bart>
2014-09-24 15:18:50 +0200

    Moved DB config to ini file

---
+++ htdocs/lib/libConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+function readConfigFiles() {
+   global $config;
+
+   $sampleConfigFile = __DIR__.'/../config/main.sample.ini';
+   $sampleConfig = file_exists($sampleConfigFile) ? parse_ini_file($sampleConfigFile) : array() ;
+   $mainConfigFile = __DIR__.'/../config/main.ini';
+   $mainConfig = file_exists($mainConfigFile) ? parse_ini_file($mainConfigFile) : array() ;
+
+   $config = array_replace($sampleConfig, $mainConfig);
+   var_dump($config);
+
+}
